7a5cfb2f9db75e4244b2522ce1bc265d471a5e59
Dionisio Tmost <dionisio@tripfab.com>
2012-01-16 14:39:32 -0600

    Clean up of the project file system

--- html/blog/wp-content/themes/premiumpixels/searchform.php
+++
@@ -1,8 +0,0 @@
-<!--BEGIN #searchform-->
-<form method="get" id="searchform" action="<?php echo home_url(); ?>/">
-	<label class="hidden" for="s"><?php _e('Search for:', 'framework'); ?></label>
-	<input type="text" value="To search type and hit enter" onfocus="if(this.value=='To search type and hit enter')this.value='';" 
-onblur="if(this.value=='')this.value='To search type and hit enter';" name="s" id="s" />
-	<input class="hidden" type="submit" id="searchsubmit" value="Search" />
-<!--END #searchform-->
-</form>
