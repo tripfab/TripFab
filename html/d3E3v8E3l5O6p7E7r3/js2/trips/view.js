0e3f8e4e0219bc81e1d4939fbb91198315c95733
Cristian Araya <cristian@tripfab.com>
2012-04-27 17:45:56 -0500

    Changes on the trips page

--- html/d3E3v8E3l5O6p7E7r3/js2/trips/view.js
+++ html/d3E3v8E3l5O6p7E7r3/js2/trips/view.js
@@ -1,4 +1,58 @@
 $(document).ready(function() {
+	$('.tabs-wrapper').tabs({
+		autoHeight: true
+	});
+	
+//	function onAfter(curr, next, opts, fwd) {
+//	var index = opts.currSlide;
+	//get the height of the current slide
+//	var $ht = $(this).height();
+	//set the container's height to that of the current slide
+//	$(this).parent().animate({height: $ht});
+//	}
+
+	
+	
+    $('#slider-1').cycle({
+    	fx:     'scrollHorz',
+    	next: 	'#next-1',
+    	prev: 	'#prev-1',
+    	pager:	'#cycle-pager-1',
+    	timeout:0,
+    	//after: onAfter
+    });
+    $('#slider-2').cycle({
+    	fx:     'scrollHorz',
+    	next: 	'#next-2',
+    	prev: 	'#prev-2',
+    	pager:	'#cycle-pager-2',
+    	timeout:0,
+    	//after: onAfter
+    });
+    $('#slider-3').cycle({
+    	fx:     'scrollHorz',
+    	next: 	'#next-3',
+    	prev: 	'#prev-3',
+    	pager:	'#cycle-pager-3',
+    	timeout:0,
+    	//after: onAfter
+    });
+    $('#slider-4').cycle({
+    	fx:     'scrollHorz',
+    	next: 	'#next-4',
+    	prev: 	'#prev-4',
+    	pager:	'#cycle-pager-4',
+    	timeout:0,
+    	//after: onAfter
+    });
+    $('#slider-5').cycle({
+    	fx:     'scrollHorz',
+    	next: 	'#next-5',
+    	prev: 	'#prev-5',
+    	pager:	'#cycle-pager-5',
+    	timeout:0,
+    	//after: onAfter
+    });
     
     $('.trip-gallery').cycle({
         pager: '#pager'
@@ -9,8 +63,6 @@ $(document).ready(function() {
         buttonPrevHTML: '<div><span>Previous Day</span></div><div><span>Previous Day</span></div>',
         buttonNextHTML: '<div><span>Next Day</span></div><div><span>Next Day</span></div>'
     });
-    
-    $('.tabs-wrapper').tabs();
     
     var aux = true;
     
@@ -31,14 +83,23 @@ $(document).ready(function() {
             $('input[name=checkin]').val(date);
         }
     });
-    
-    $("a.lb").fancybox({
-        padding: 0,
-        showCloseButton: false,
-        overlayOpacity: 0.8,
-        overlayColor: '#FFFFFF',
-        centerOnScroll:true
-    });
+	
+	$(".slideshow .cont").jcarousel({
+		scroll: 1
+	});
+	
+	$('a.lbc, a.lb').fancybox({
+		padding: 0,
+		overlayColor: '#FFF',
+		overlayOpacity: '0.7',
+		showCloseButton: 'false',
+		centerOnScroll: 'true',
+		titlePosition: 'inside'
+	});
+	
+	$('#fancybox-title').addClass('tittle');
+	$('#fancybox-left-ico').addClass('lfarrow');
+	$('#fancybox-right-ico').addClass('rgarrow');
 	
     $('select[name=adults]').change(function(){
         var $limit = $('option:last',this).val() - $(this).val();
